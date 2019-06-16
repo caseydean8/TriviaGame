03fe7847fb0a18c818d8fd6bf28289cbaa2905e3
Casey Carroll <caseydean8@gmail.com>
2019-06-16 18:00:18 -0400

    Added on click event to populate question field.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,18 @@
 // Create global variable for correct answers, incorrect answers, and unanswered.
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var unanswered = 0;
 
+var questions = "Which movie won the special achievement award at the 60th Academy Awards?"
 
 // Use onclick event to populate question field and answer buttons.
+function startGame() {
+    // start 15s timer.
+    document.getElementById("question").innerHTML = questions;
+    console.log(questions);
+}
 
 
-// start 15s timer.
 
 
 // If player guesses before timer expires.
