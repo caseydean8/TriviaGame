5228781d5e9b74f699548f160c3387b1f0212fc5
Casey Carroll <caseydean8@gmail.com>
2019-06-16 16:54:21 -0400

    Pseudocoded js file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// Create global variable for correct answers, incorrect answers, and unanswered.
+
+
+// Use onclick event to populate question field and answer buttons.
+
+
+// start 15s timer.
+
+
+// If player guesses before timer expires.
+
+
+// Alert player if they are correct or incorrect and show correct answer. 
+
+
+// If timer expires show correct answer.
+
+
+// Keep track of players correct guesses, incorrect guesses, and timer expiration.
+
+
+// Load next screen with timer ~4s.
+
+
+// After last question, show correct, incorrect, and unanswered(timer expiration) tallies. Also create a start over button that resets the game.
