7d28736b182f1509a88b166ee583f1a37dcc6ac3
Casey Carroll <caseydean8@gmail.com>
2020-01-31 11:15:28 -0500

    Gif resets after setTimeout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 // Create global variable for correct answers, incorrect answers, and unanswered.
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
-var page = 0;
-var rightAnswer;
-var gameQuestions = {
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+let unanswered = 0;
+let page = 0;
+let rightAnswer;
+const game = {
   questions: [
     "Which movie won the special achievement award at the 60th Academy Awards?",
     "Which supernatural thrillers protagonist was named Jack Burton?",
@@ -22,36 +22,36 @@ var gameQuestions = {
   ],
   rightAnswer: ["Robocop", "Big Trouble in Little China", "Bryan Bozworth"],
   gif: [
-    `https://media.giphy.com/media/4EFuzRi9HpbW/giphy.gif`,
-    "https://giphy.com/gifs/iY9p9jXXUq4YWFy3g3/html5",
+    "https://media.giphy.com/media/4EFuzRi9HpbW/giphy.gif",
+    "https://media.giphy.com/media/69lXr3CqBmNaTypTjC/giphy.gif",
     "https://66.media.tumblr.com/c7eb4eb6b04d68b1c5c534c41ff3f41c/tumblr_pfxpvvEBW81snghrzo1_400.gif"
   ]
 };
 
 // Use onclick event to populate question field and answer buttons.
 const startGame = () => {
-  // $("#start-button").remove();
+  $(".gif-hold").remove();
   document.getElementById("start-button").style.display = "none";
   document.getElementById("timer").style.display = "flex";
   document.getElementById("timer").innerHTML = "15";
   document.getElementById("answer-output").innerHTML = "";
-  document.getElementById("question").innerHTML = gameQuestions.questions[page];
+  document.getElementById("question").innerHTML = game.questions[page];
 
-  for (var i = 0; i < gameQuestions.answers[page].length; i++) {
+  for (let i = 0; i < game.answers[page].length; i++) {
     const answerButton = document.createElement("button");
     answerButton.classList.add("btn");
-    answerButton.innerHTML = gameQuestions.answers[page][i];
+    answerButton.innerHTML = game.answers[page][i];
     $("div.button").append($(answerButton));
   }
   // start 15s timer.
   run();
 };
 
-var intervalId;
+const intervalId;
 const run = () => {
   clearInterval(intervalId);
   intervalId = setInterval(decrement, 1000);
-  var countdownNumber = 15;
+  let countdownNumber = 15;
 
   function decrement() {
     //  Decrease number by one.
@@ -67,7 +67,7 @@ const run = () => {
       document.getElementById("question").innerHTML = "";
 
       document.getElementById("question").innerHTML =
-        "The answer is " + gameQuestions.rightAnswer[page];
+        "The answer is " + game.rightAnswer[page];
 
       reset();
     }
@@ -77,38 +77,32 @@ const run = () => {
 // Create a listener for question button click
 $("div.button").on("click", "button.btn", function() {
   var fired_button = this.innerHTML;
-  const gifHold = $("<img>");
-  document.getElementById("question").innerHTML = "";
+  document.getElementById("question").textContent = "";
 
-  if (fired_button === gameQuestions.rightAnswer[page]) {
+  if (fired_button === game.rightAnswer[page]) {
     document.getElementById("question").textContent =
       "Correct! The answer is " + $(this).text();
-    $(gifHold).attr("src", gameQuestions.gif[page]);
-    // document.getElementById("button").outerHTML = gifHold;
-    $(".button").append(gifHold);
     correctAnswers++;
-
-    reset();
   } else {
     document.getElementById("question").innerHTML =
-      "Incorrect! The answer is " + gameQuestions.rightAnswer[page];
+      "Incorrect! The answer is " + game.rightAnswer[page];
     incorrectAnswers++;
-
-    reset();
   }
+  reset();
 });
 
 const reset = () => {
   $(".btn").remove();
+  const gifHold = $("<img>");
+  gifHold.addClass("gif-hold");
+  $(gifHold).attr("src", game.gif[page]);
+  $(".button").append(gifHold);
   page++;
-
   clearInterval(intervalId);
   // Set delay for next question.
-  if (page === gameQuestions.questions.length) {
-    setTimeout(gameReset, 3000);
-  } else {
-    setTimeout(startGame, 3000);
-  }
+  page === game.questions.length
+    ? setTimeout(gameReset, 3000)
+    : setTimeout(startGame, 3000);
 };
 
 const gameReset = () => {
@@ -116,20 +110,14 @@ const gameReset = () => {
   document.getElementById("timer").innerHTML = "";
   document.getElementById("question").innerHTML = "";
   document.getElementById("answer-output").innerHTML = "";
-
-  // const summary = ("<p>", { id: "correct-answers" });
   const summary = "<p>";
-  // var $incorrectDisplay = ("<div>", { id: "incorrect-answers" });
-  var good = correctAnswers;
-  var bad = incorrectAnswers + unanswered;
+  const good = correctAnswers;
+  const bad = incorrectAnswers + unanswered;
 
   $("#question").append(
     (summary.innerHTML = `You got ${good} correct, and you got ${bad} wrong.`)
   );
 
-  // $("#right").append(
-  //   ($incorrectDisplay.innerHTML = "You got " + bad + " wrong.")
-  // );
   // ------- RESET BUTTON -------
   document.getElementById("timer").style.display = "none";
   const resetButton = document.createElement("button");
@@ -149,9 +137,5 @@ const clearAnswerTally = () => {
   correctAnswers = 0;
   incorrectAnswers = 0;
   unanswered = 0;
-
-  // document.getElementById("right").innerHTML = "";
-  // document.getElementById("wrong").innerHTML = "";
-
   startGame();
 };
